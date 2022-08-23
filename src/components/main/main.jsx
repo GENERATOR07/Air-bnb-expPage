8f34b202130167dba6c2e082cbacbde8a41281ea
GENERATOR07 <1998shubhamrex@gmail.com>
2022-08-23 20:37:53 +0530

    added Nav and Main components with css

---
+++ src/components/main/main.jsx
@@ -0,0 +1,12 @@
+const Main=()=>{
+    return(
+        <div className="Main">
+      <img src="./Group 77.png"/>
+      <h1>Online Experiences</h1>
+      <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      
+    </div>
+
+    )
+}
+export default Main;
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,9 @@
+ const Nav=()=>{
+    return (
+        <div className="nav">
+            <img src="./logo.png" alt="abn  logo"></img>
+            
+        </div>
+    )
+}
+export default Nav;
